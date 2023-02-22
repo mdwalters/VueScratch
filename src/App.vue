aae19a848eb7130b384b0993bde9de244d9b7106
M.D. Walters <67456566+mdwalters@users.noreply.github.com>
2023-02-22 16:25:33 +0000

    Add files

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+    import { onMounted, ref } from "vue";
+    import JSZip from "jszip";
+
+    let jszip;
+    let canvas;
+    let app;
+    let loaded = ref(false);
+
+    onMounted(() => {
+        jszip = new JSZip();
+        
+        app = document.getElementById("app");
+        canvas = document.getElementById("canvas").getContext("2d");
+        
+        canvas.fillStyle = "#fcfcfc";
+        canvas.fillRect(0, 0, 480, 360);
+    });
+</script>
+
+<template>
+    <h1>VueScratch</h1>
+    <p>VueScratch is a Scratch project runner written in Vue</p>
+    
+    <canvas id="canvas" width="480" height="360"></canvas>
+    
+    <div>
+        <button id="start">Start</button>
+        <button id="stop">Stop</button>
+    </div>
+
+    <div v-if="!loaded" class="container warning">
+        <p>A project hasn't been loaded yet</p>
+    </div>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+import "./lib/index.css";
+
+createApp(App).mount("#app");
