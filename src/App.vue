2aa18561efbfd52cb8e8344dcb4f6f065eb1859d
M.D. Walters <popularvsofficial@gmail.com>
2023-02-23 21:20:57 -0500

    h

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
     import { onMounted, ref } from "vue";
-    import JSZip from "jszip";
     
     let canvas;
     let app;
+
+    let stage_width = ref(480);
+    let stage_height = ref(360); 
 
     onMounted(() => {
         app = document.getElementById("app");
@@ -18,15 +20,11 @@
     <h1>VueScratch</h1>
     <p>VueScratch is a Scratch project runner written in Vue. It is currently experimental.</p>
 
-    <canvas id="canvas" width="480" height="360"></canvas>
+    <canvas id="canvas" width=stage_width height=stage_height></canvas>
     
     <div>
-        <button id="start" onclick="alert('Projects can\'t be started yet')">Start</button>
-        <button id="stop" onclick="alert('Projects can\'t be stopped yet')">Stop</button>
-        <button id="upload" onclick="alert('Projects can\'t be started yet')">Upload</button>
-    </div>
-    
-    <div class="container">
-        <p>A project hasn't been loaded yet.</p>
+        <button id="start">Start</button>
+        <button id="stop">Stop</button>
+        <input type="number" id="project-id" class="container">
     </div>
 </template>
